553ce7a1f0b1243be720ab0e1bee3a9af646ce4b
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-04-12 22:57:11 +0200

    refactor AlertButton

---
+++ Bisect/AlertButtonView.swift
@@ -0,0 +1,36 @@
+//
+//  AlertButtonView.swift
+//  Bisect
+//
+//  Created by Antonio on 12/04/23.
+//
+
+import SwiftUI
+
+struct AlertButtonView: View {
+    
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        Button(action: {
+            isPresented.toggle()
+        }) {
+            Text("THE BUTTON")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.init(uiColor: .systemBlue))
+                .cornerRadius(20)
+                .shadow(radius: 5, y: 8)
+        }
+        .alert("GIT BISECT WORKING!", isPresented: $isPresented, actions: {
+            Button(action: { }) { Text("Close") }
+        })
+        
+    }
+}
+
+struct AlertButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertButtonView(isPresented: .constant(false))
+    }
+}
--- Bisect/ContentView.swift
+++ Bisect/ContentView.swift
@@ -12,22 +12,7 @@ struct ContentView: View {
     @State private var alertShowed: Bool = false
     
     var body: some View {
-        VStack {
-            Button(action: {
-                alertShowed.toggle()
-            }) {
-                Text("THE BUTTON")
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.init(uiColor: .systemBlue))
-                    .cornerRadius(20)
-                    .shadow(radius: 5, y: 8)
-            }
-        }
-        .alert("GIT BISECT WORKING!", isPresented: $alertShowed, actions: {
-            Button(action: { }) { Text("Close") }
-        })
-        .padding()
+        AlertButtonView(isPresented: $alertShowed)
     }
 }
 
