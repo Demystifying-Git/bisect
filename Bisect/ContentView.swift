1caff9a0a2c15037158e13fead17de27a4818b93
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-04-12 23:11:21 +0200

    add info tab

--- Bisect/ContentView.swift
+++ Bisect/ContentView.swift
@@ -10,23 +10,48 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var alertShowed: Bool = false
+    @State private var selectedTab: Int = 1
+    
+    init() {
+        UITabBar.appearance().backgroundColor = .systemBackground
+    }
     
     var body: some View {
-        NavigationView {
-            GeometryReader { geo in
-                ScrollView {
-                    VStack {
-                        AlertButtonView(isPresented: $alertShowed)
+        TabView(selection: $selectedTab) {
+            NavigationView {
+                GeometryReader { geo in
+                    ScrollView {
+                        VStack {
+                            AlertButtonView(isPresented: $alertShowed)
+                        }
+                        .frame(
+                            width: geo.frame(in: .local).width,
+                            height: geo.frame(in: .local).height,
+                            alignment: .center
+                        )
                     }
-                    .frame(
-                        width: geo.frame(in: .local).width,
-                        height: geo.frame(in: .local).height,
-                        alignment: .center
-                    )
                 }
+                .background(Color(uiColor: .systemGroupedBackground))
+                .navigationTitle("Git bisect")
+            }.tabItem {
+                Text("Home")
+                Image(systemName: "house")
+            }.tag(0)
+            
+            NavigationView {
+                List {
+                    Section {
+                        Text("This is an example app that shows how git bisect works")
+                    } footer: { Text("Git is good!") }
+                }
+                .navigationTitle("Info")
             }
-            .navigationTitle("Git bisect")
+            .tabItem {
+                Text("Info")
+                Image(systemName: "info.circle")
+            }.tag(1)
         }
+        
     }
 }
 
